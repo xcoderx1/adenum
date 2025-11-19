59bab27cde9cb6d6acf871eac8a1ee74ee25232d
Murat Sahin <64471952+xcoderx1@users.noreply.github.com>
2025-11-19 13:09:16 +0000

    Delete modules directory

--- modules/05_gpo.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-run_gpo_enum() {
-    echo -e "${YELLOW}PHASE 5: GPO Enumeration${NC}"
-    run_ldap "(objectClass=groupPolicyContainer)" "gpo/all_gpos.ldif" "Enumerating GPOs"
-    run_ldap "(gPLink=*)" "gpo/gpo_links.ldif" "Finding GPO links"
-    # Add: SYSVOL credential hunting with smbclient
-    echo ""
-}
